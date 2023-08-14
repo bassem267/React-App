8c8485d23582a6937ba3372fb65b639c7ca72ddb
Bassem Ismail <74353081+bassem267@users.noreply.github.com>
2023-08-14 18:15:23 +0300

    Props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
 import { MouseEvent, useState } from "react";
 
-function ListGroup() {
-  let items = ["Paris", "Tokyo", "London", "New York"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectedItem: (item: string) => void;
+}
 
+function ListGroup({ items, heading, onSelectedItem}: Props) {
   const [selectedItem, setSelectedItem] = useState(-1);
 
   const handelClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
-      <h1>City List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.length === 0 && (
           <li className="list-group-item">No Item Found</li>
@@ -19,11 +23,12 @@ function ListGroup() {
             className={
               selectedItem === index
                 ? "list-group-item active"
-                : "list-group-item active"
+                : "list-group-item "
             }
             key={item}
             onClick={() => {
               setSelectedItem(index);
+              onSelectedItem(item);
             }}
           >
             {item}
