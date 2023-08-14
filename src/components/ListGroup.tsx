95247a95694da56ba2c5b04e599e13ae3d4c54e8
bassem267 <bassem8mohamed8@gmail.com>
2023-08-14 14:49:14 +0300

    sync

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,37 @@
+import { MouseEvent, useState } from "react";
+
+function ListGroup() {
+  let items = ["Paris", "Tokyo", "London", "New York"];
+
+  const [selectedItem, setSelectedItem] = useState(-1);
+
+  const handelClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>City List</h1>
+      <ul className="list-group">
+        {items.length === 0 && (
+          <li className="list-group-item">No Item Found</li>
+        )}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedItem(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
